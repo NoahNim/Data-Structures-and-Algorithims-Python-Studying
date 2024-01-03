e95f4e75a587bb4c6051f7045c40acf9f1c0ed12
Noah Medoff <noahmedoff@gmail.com>
2024-01-03 11:28:35 -0800

    linked list traversal both iteratively and recursively

---
+++ StructyProblems/linkedlistraversal.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+a = Node("A")
+b = Node("B")
+c = Node("C")
+d = Node("D")
+
+a.next = b
+b.next = c
+c.next = d
+
+# A -> B -> C -> D -> None
+
+
+def print_list(head):
+    current = head
+    while current is not None:
+        print(current.val)
+        current = current.next
+
+
+# print_list(a)
+
+def print_list_recursion(head):
+    if head is None:
+        return
+    print(head.val)
+    print_list_recursion(head.next)
+
+
+# print_list_recursion(a)
