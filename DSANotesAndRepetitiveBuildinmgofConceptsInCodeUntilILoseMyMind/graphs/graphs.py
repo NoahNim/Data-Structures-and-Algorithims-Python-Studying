51e3c7045b37cecf7873ce8a5f1d65bc33fa35f0
noahmedoff@gmail.com <noahmedoff@gmail.com>
2024-04-13 14:25:13 -0700

    some graph notes

---
+++ DSANotesAndRepetitiveBuildinmgofConceptsInCodeUntilILoseMyMind/graphs/graphs.py
@@ -0,0 +1,8 @@
+# Graphs are a non-linear data structure consisting of vertices and adges. The vertices are sometimes referred to as nodes and the edges are lies or arcs that connect nay two nodes in a graph.
+# A graph is composed of a set of vertices (V) and a set of edges (E). The graph can be denoted as G(V,E)
+
+# 
+#    Vertices: Vertices are the fundamental units of the graph. Sometimes, vertices are also known as vertex or nodes. Every node/vertex can be labeled or unlabeled.
+#   Edges: Edges are drawn or used to connect two nodes of the graph. It can be ordered pair of nodes in a directed graph. Edges can connect any two nodes in any possible way. There are no rules. Sometimes, edges are also known as arcs. Every edge can be labelled/unlabelled.
+
+# Graphs can have cycles and disconnected components. Trees have no cycle and are heirarchical.
