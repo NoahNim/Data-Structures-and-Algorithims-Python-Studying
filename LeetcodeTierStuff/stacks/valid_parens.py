27749b4d88a89901c281c6b72fc14d1e8420a8e2
noahmedoff@gmail.com <noahmedoff@gmail.com>
2024-04-16 15:21:41 -0700

    valid parentheses problem

--- LeetcodeTierStuff/stacks/valid_parens.py
+++ LeetcodeTierStuff/stacks/valid_parens.py
@@ -5,14 +5,34 @@
 # Does string only contain ()[]{}?
 
 # Plan
-#   stack = deque()
-#   open = hash of "({["
-#   closed = hash of ")}]"
-#   i = 0
+#   stack = deque([])
+# pairs = {} a hash map of open and closing parentheses
 # Add to stack while char is an open parenthesis
-# for char in s:
-#   if char in open:
-#       stack.push(char)
-#   else:
-#       closer = closed.index(char)
-#       
+#  iterate through list for char in s:
+    # check if char in pairs
+    #   if char in pairs:
+    #       stack.push(char)
+    #  check if stack length is 0 or char is not paired to the bracket popped from stack return False if those cases are hit
+    #   if len(stack) == 0 or char != pairs[stack.pop()]
+    #       return False
+# check if stack length is 0, if it is that means the loop succesfully iterated finding the correct matching pairs so return True
+# otherwise False can be return
+
+from collections import deque
+
+def bracket_sequence_validator(s):
+    stack = deque([])
+    pairs = {
+        "(": ")",
+        "{": "}",
+        "[": "]",
+    }
+    for char in s:
+        if char in pairs:
+            stack.append(char)
+        elif len(stack) == 0 or char != pairs[stack.pop()]:
+            return False
+    if len(stack) == 0:
+        return True
+    else:
+        return False
